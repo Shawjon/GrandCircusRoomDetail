26b069327105643e6effd22eb3d9762757179101
Jon Shaw <Jonshaw@quickenloans.com>
2018-10-09 11:12:40 -0400

    updated interface language

--- GrandCircusRoomDetail/Program.cs
+++ GrandCircusRoomDetail/Program.cs
@@ -19,17 +19,18 @@ namespace GrandCircusRoomDetail
                 string widthString, lengthString;
                 Console.WriteLine("Welcome to the Grand Circus' Room Detail Generator");
 
-                Console.Write("Please enter the width of the room: ");
-                widthString = Console.ReadLine();
-                width = double.Parse(widthString);
-                Console.Write("Please enter the length of the room: ");
+               
+                Console.Write("Enter Length: ");
                 lengthString = Console.ReadLine();
                 length = double.Parse(lengthString);
+                Console.Write("Enter Width: ");
+                widthString = Console.ReadLine();
+                width = double.Parse(widthString);
 
                 area = (length * width);
                 perimeter = ((length * 2) + (width * 2));
-                Console.WriteLine("The Area of the Room is: " + area);
-                Console.WriteLine("The perimeter of the room is: " + perimeter);
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
                 Console.Write("Do you want to continue? Yes or No: ");
                 var entry = Console.ReadLine();
                 if (entry.ToLower() == "no")
